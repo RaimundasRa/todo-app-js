f7ac44bf046b2ef1d49b6516f2aa9169e2cf4d4d
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-05-10 14:21:03 +0100

    add task button functionality

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('hello');
+//Slectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+function addTodo(event) {
+  //Prevent form from submiting
+  event.preventDefault();
+  //Todo DIV
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  //Create LI
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'yo!';
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  //Check mark button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+  //Check trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('complete-btn');
+  todoDiv.appendChild(trashButton);
+  //Append to list
+  todoList.appendChild(todoDiv);
+}
